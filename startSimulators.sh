230e3be903c0c449b1fa8a107941959e7c8ce5f4
kvandegrift <kvandegrift@suter-lab2.ltcengineering.com>
2015-08-25 15:53:05 -0400

    Corrected config files.

--- startSimulators.sh
+++ startSimulators.sh
@@ -24,11 +24,11 @@ fi
 curl http://10.1.10.51:9011/startSimulator >> results_51_9011.log &
 curl http://10.1.10.51:9012/startSimulator >> results_51_9012.log &
 curl http://10.1.10.23:9031/startSimulator >> results_23_9031.log &
-#curl http://10.1.10.23:9032/startSimulator >> results_23_9032.log &
-#curl http://10.1.10.24:9041/startSimulator >> results_24_9041.log &
+curl http://10.1.10.23:9032/startSimulator >> results_23_9032.log &
+curl http://10.1.10.24:9041/startSimulator >> results_24_9041.log &
 curl http://10.1.10.24:9042/startSimulator >> results_24_9042.log &
 
 wait
 
-cat results_{51_9011,24_9042,51_9012,23_9031}.log > results_all.log
+cat results_{51_9011,24_9042,51_9012,23_9031,23_9032,24_9041}.log > results_all.log
 #cat results_51_9000.log > results_all.log
